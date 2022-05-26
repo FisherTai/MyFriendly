bea431d4e0c3a2d78a46393e66a30345375183fd
LAPTOP-1SGCLO1T\Fisher <unifisher02@gmail.com>
2022-05-26 15:28:26 +0800

    style bug fix

--- public/client/src/utils/untils.ts
+++ public/client/src/utils/untils.ts
@@ -1,6 +1,11 @@
 import { strings } from "../config/strings";
 
 export const isDaylightMode = (): boolean => {
+  
+  if (!localStorage.getItem(strings.LOCAL_STORAGE_STYLE_MODE)) {
+    return true;
+  }
+
   return (
     localStorage.getItem(strings.LOCAL_STORAGE_STYLE_MODE) ===
     strings.STYLE_MODE_DAYLIGHT
