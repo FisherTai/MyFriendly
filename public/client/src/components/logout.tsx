372d1e107e8e9cdc258e4ed5f858889dea65d6ae
LAPTOP-1SGCLO1T\Fisher <unifisher02@gmail.com>
2022-06-01 20:44:10 +0800

    react-cookie remove

--- public/client/src/components/logout.tsx
+++ public/client/src/components/logout.tsx
@@ -8,12 +8,10 @@ import { strings } from "../config/strings";
 import { componentProps } from "../config/style-mode-interface";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import { useCookies } from "react-cookie";
 type Props = {};
 
 const Logout = (props: Props) => {
   const navigate = useNavigate();
-  const [cookies, setCookie, removeCookie] = useCookies<string>(["jwt"]);
   const variableStyle = useSelector(
     (state: RootState) => state.styleMode.value
   );
@@ -25,7 +23,6 @@ const Logout = (props: Props) => {
     const data = await axios.get(`${logoutRoute}/${id}`, { withCredentials: true });
     if (data.status === 200) {
       localStorage.clear();
-      removeCookie("jwt");
       navigate("/login");
     }
   };
